437638fc6a31646d7efd75ee119c49632d14e10b
Daniel <daniel@Daniels-MacBook-Pro-10.local>
2024-11-16 17:45:15 -0700

    update

--- science/blobs/catalog/catalog.js
+++ science/blobs/catalog/catalog.js
@@ -13,9 +13,9 @@ $(document).ready(function() {
             row += '<td>' + obj.field_name + '</td>';
             row += '<td>' + (obj.ra !== null && obj.ra !== undefined ? obj.ra.toFixed(5) : 'N/A') + '</td>';
             row += '<td>' + (obj.dec !== null && obj.dec !== undefined ? obj.dec.toFixed(5) : 'N/A') + '</td>';
-            row += '<td>' + (obj.magnitude_G !== null && obj.magnitude_G !== undefined ? obj.magnitude_G : 'N/A') + '</td>';
-            row += '<td>' + (obj.magnitude_R !== null && obj.magnitude_R !== undefined ? obj.magnitude_R : 'N/A') + '</td>';
-            row += '<td>' + (obj.magnitude_I !== null && obj.magnitude_I !== undefined ? obj.magnitude_I : 'N/A') + '</td>';
+            row += '<td>' + (obj.magnitudes_G !== null && obj.magnitudes_G !== undefined ? obj.magnitudes_G : 'N/A') + '</td>';
+            row += '<td>' + (obj.magnitudes_R !== null && obj.magnitudes_R !== undefined ? obj.magnitudes_R : 'N/A') + '</td>';
+            row += '<td>' + (obj.magnitudes_I !== null && obj.magnitudes_I !== undefined ? obj.magnitudes_I : 'N/A') + '</td>';
             row += '<td>' + (obj.redshift !== null && obj.redshift !== undefined ? obj.redshift : 'N/A') + '</td>';
             row += '<td>' + (obj.luminosity !== null && obj.luminosity !== undefined ? obj.luminosity.toExponential(2) : 'N/A') + '</td>';
             row += '<td>' + (obj.equivalent_width !== null && obj.equivalent_width !== undefined ? obj.equivalent_width : 'N/A') + '</td>';
@@ -23,19 +23,17 @@ $(document).ready(function() {
             // Display the compiled image with a modal
             row += '<td>';
             if (obj.image_filename) {
-                row += '<a href="#" data-toggle="modal" data-target="#imageModal' + index + '">';
+                row += '<a href="#" data-bs-toggle="modal" data-bs-target="#imageModal' + index + '">';
                 row += '<img src="../../../' + obj.image_filename + '" alt="' + obj.object_name + '" class="img-thumbnail" width="150">';
                 row += '</a>';
 
                 // Modal structure
-                row += '<div class="modal fade" id="imageModal' + index + '" tabindex="-1" role="dialog" aria-labelledby="modalLabel' + index + '" aria-hidden="true">';
-                row += '  <div class="modal-dialog modal-lg" role="document">';
+                row += '<div class="modal fade" id="imageModal' + index + '" tabindex="-1" aria-labelledby="modalLabel' + index + '" aria-hidden="true">';
+                row += '  <div class="modal-dialog modal-lg">';
                 row += '    <div class="modal-content">';
                 row += '      <div class="modal-header">';
                 row += '        <h5 class="modal-title" id="modalLabel' + index + '">' + obj.object_name + '</h5>';
-                row += '        <button type="button" class="close" data-dismiss="modal" aria-label="Close">';
-                row += '          <span aria-hidden="true">&times;</span>';
-                row += '        </button>';
+                row += '        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>';
                 row += '      </div>';
                 row += '      <div class="modal-body text-center">';
                 row += '        <img src="../../../' + obj.image_filename + '" alt="' + obj.object_name + '" class="img-fluid">';
@@ -43,7 +41,6 @@ $(document).ready(function() {
                 row += '    </div>';
                 row += '  </div>';
                 row += '</div>';
-
             } else {
                 row += 'No image available';
             }
